83a18754b61b3722976552c96ce7a3da183ff006
Brian.L.Pedersen <Brian.L.Pedersen@PC17001.corp.local>
2013-04-16 09:21:45 +0200

    Added a main class - MISSION CONTROL!!! PREPARE FOR LAUNCH (..of Skynet)

---
+++ src/pathfinding/MissionControl.java
@@ -0,0 +1,13 @@
+package pathfinding;
+
+public class MissionControl {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String path = "Graphset.txt";
+		Graph graphical = new Graph(path);
+	}
+
+}
